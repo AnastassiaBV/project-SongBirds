32cd7d627ebe8d70e99d5b9cde34cb443d42c61d
AnastassiaBV <bv.anastassy@gmail.com>
2023-01-26 15:16:14 +0300

    add page2 css and html file

---
+++ js/code.js
@@ -0,0 +1,33 @@
+const updateContent = (id) => {
+    let listOfItems = "";
+
+    for (const item of birdsData[id]) {
+        listOfItems = listOfItems + generateItem(item);
+    }
+
+    const documentEl = document.getElementById("birds_content");
+    console.log(documentEl)
+
+    documentEl.innerHTML = `<div>${listOfItems}</div>`;
+
+    console.log(listOfItems)
+    // const fragment = document.createDocumentFragment();
+    // const pEl = document.createElement('p');
+    // pEl.textContent = "TTT sdfglksdfglksnd;fgjkl";
+    // fragment.append(pEl);
+
+    // documentEl.append(fragment);
+
+    // console.log("III :: " + JSON.stringify(documentEl.innerHTML));
+    // window.onload;
+}
+
+const generateItem = (data) => {
+    
+    return `<div 
+    id="${data.id}" >` + data.name + `<br>` + 
+        `<p>${data.species}</p><br>` + 
+        `<p>${data.description}</p>` + `<br>` + 
+        `<img src="${data.image}" alt="${data.name}" />` +
+        `<audio controls src="${data.audio}" type="audio/mpeg"> </audio> </div>`;
+}
